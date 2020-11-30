e805779c43476f5d45eb2acdf02fec5d875500a1
Shu Matsumoto <shumatsumoto0328@gmail.com>
2020-11-30 09:12:40 +0900

    Header作成　CSS適応

---
+++ components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+	<div id="header">
+		<div class="appNameWrapper">
+			<span>Todo App</span>
+		</div>
+		<div class="addNew">
+			<span>新規作成</span>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
